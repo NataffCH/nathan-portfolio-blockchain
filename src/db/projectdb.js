afe8636c914448a858fa8f19ce24d6a17add9a91
Neymar <NathanAffolter@proton.me>
2024-07-04 02:16:38 -0700

    Modify projects

--- src/db/projectdb.js
+++ src/db/projectdb.js
@@ -27,8 +27,6 @@ export const ProjectList = [
     description:
       "Cryptocurrency trading bot is a bot designed specifically for trading cryptocurrencies. These bots are programmed to analyze market data, identify trading opportunities, and execute trades on cryptocurrency exchanges.",
     techStack: [node, cryptocurrency],
-    liveLink: "https://marvelous-crack-5191.vercel.app/",
-    gitLink: "https://github.com/SumatM/Spartan",
   },
   {
     heading: "Sniping Bot",
@@ -37,8 +35,6 @@ export const ProjectList = [
     description:
       "Sniping bots are a type of automated software program designed to execute trades at specific price levels or market conditions. These bots are commonly used in cryptocurrency and stock trading to take advantage of sudden price movements or to enter or exit positions quickly.",
     techStack: [node, express, react],
-    liveLink: "https://puny-potato-4945.vercel.app/",
-    gitLink: "https://github.com/SumatM/Arivu",
   },
   {
     heading: "Stock Bot",
@@ -47,7 +43,5 @@ export const ProjectList = [
     description:
       "Stock bot is a bot that can refer to any type of bot related to stocks. This could include trading bots, but it could also refer to bots that perform other functions, such as managing portfolios, tracking stock prices, or providing market analysis.",
     techStack: [python, cryptocurrency],
-    liveLink: "https://indian-states.netlify.app/index.html",
-    gitLink: "https://github.com/SumatM/Indian-States",
   },
 ];
